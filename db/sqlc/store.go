5c33f898706e68bd18c6f6622b57e8c6513af856
nima abdpoor <nimaabdpoor@gmail.com>
2022-10-13 19:52:23 +0330

    create store class for running transactions.

---
+++ db/sqlc/store.go
@@ -0,0 +1,88 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type store struct {
+	q  *Queries
+	db *sql.DB
+}
+
+func newStore(db *sql.DB) *store {
+	return &store{
+		q:  New(db),
+		db: db,
+	}
+}
+
+// execute a function within a database transaction.
+func (store *store) execTx(ctx context.Context, fn func(queries *Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfers `json:"transfer"`
+	FromAccount Accounts  `json:"from_account"`
+	ToAccount   Accounts  `json:"to_account"`
+	FromEntry   Entries   `json:"from_entry"`
+	ToEntry     Entries   `json:"to_entry"`
+}
+
+func (store store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(queries *Queries) error {
+		var err error
+		result.Transfer, err = queries.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = queries.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = queries.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		//TODO Update Account Balance
+		return nil
+	})
+
+	return result, err
+}
